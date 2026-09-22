dce8d2ba860960209259dba38936041976b4ed09
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2026-09-22 15:49:42 +0300

    add interceptors

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  headers: {
-    'ngrok-skip-browser-warning': 'true',
-  },
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(config => {
+  config.params = {
+    ...config.params,
+    'ngrok-skip-browser-warning': 'true',
+  };
+  return config;
 });
